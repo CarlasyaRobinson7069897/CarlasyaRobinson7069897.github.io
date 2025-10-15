58e43e31c4740ceecf56cadc2f378e6d08904e7f
CarlasyaRobinson7069897 <cjr7069897@lpssonline.com>
2025-10-15 15:04:57 +0000

    todo 2 complete

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,11 +31,11 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-createPlatform(800,100,300,15);
+createPlatform(800,160,300,15);
 createPlatform(400,620,267,13);
 createPlatform(800,500,241,17);
-createPlatform(400,470,200,14);
-createPlatform(400,350,200,12);
+createPlatform(400,425,300,12);
+createPlatform(620,290,250,15)
 
 
 
